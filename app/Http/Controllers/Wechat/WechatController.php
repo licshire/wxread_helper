303ea1d3e653c762c21c6957deb4a499d8bc19c9
蔺晨 <linchen@51huxin.com>
2017-06-30 13:47:56 +0800

    up

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -8,6 +8,8 @@ use Log;
 use EasyWeChat\Foundation\Application;
 use NotificationChannels\Dayusms\DayusmsMessage;
 
+
+
 class WechatController extends Controller
 {
     public function serve(Request $request){
@@ -78,6 +80,55 @@ class WechatController extends Controller
         //$wechatServer = EasyWeChat::server(); // 服务端
         //$wechatUser = EasyWeChat::user(); // 用户服务
         // ... 其它同理
+//        define("TOP_SDK_WORK_DIR", "/tmp/");
+//        $topClient = new \TopClient(
+//            'LTAICjvEXLFwNeJO',
+//            'aWGmBwkRq22vNHgnDIGsIkvA0jxODn'
+//        );
+//        $smsRequest = new \AlibabaAliqinFcSmsNumSendRequest();
+//        $smsRequest->setSmsType('normal');
+//        $smsRequest->setSmsFreeSignName('微读小助');
+//        //$smsRequest->setExtend('1');
+//        $smsRequest->setSmsTemplateCode('SMS_74510046');
+//        $smsRequest->setRecNum('18310970216');
+//        if(!empty($params)){
+//            $smsRequest->setSmsParam(json_encode(['code'=>'11111', 'product'=>'读书']));
+//        }
+//        $response = $topClient->execute($smsRequest);
+//        var_dump($response);
+
+        //此处需要替换成自己的AK信息
+        $accessKeyId = "LTAICjvEXLFwNeJO";
+        $accessKeySecret = "aWGmBwkRq22vNHgnDIGsIkvA0jxODn";
+        //短信API产品名
+        $product = "微读小助";
+        //短信API产品域名
+        $domain = "dysmsapi.aliyuncs.com";
+        //暂时不支持多Region
+        $region = "cn-hangzhou";
+
+        //初始化访问的acsCleint
+        $profile = \DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
+        \DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
+        $acsClient= new \DefaultAcsClient($profile);
+
+        $request = new \Dysmsapi\Request\V20170525\SendSmsRequest;
+        //必填-短信接收号码
+        $request->setPhoneNumbers("18310970216");
+        //必填-短信签名
+        $request->setSignName("微读小助");
+        //必填-短信模板Code
+        $request->setTemplateCode("SMS_74510046");
+        //选填-假如模板中存在变量需要替换则为必填(JSON格式)
+        $request->setTemplateParam(json_encode(['code'=>'11111', 'product'=>'读书']));
+        //选填-发送短信流水号
+        //$request->setOutId("1234");
+
+        //发起访问请求
+        $acsResponse = $acsClient->getAcsResponse($request);
+        var_dump($acsResponse);
+
+
     }
 
     public function test(Request $request){
