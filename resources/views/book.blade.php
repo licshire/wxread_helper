638dc95c2165defef69f13268c79f545f3eb18e1
蔺晨 <linchen@51huxin.com>
2017-07-15 18:11:53 +0800

    up

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -155,6 +155,9 @@
     wx.config(<?php echo $js->config(array('chooseImage', 'previewImage','uploadImage','downloadImage'), true) ?>);
     wx.ready(function(){
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        wx.hideMenuItems({
+            menuList: ["menuItem:share:appMessage", "menuItem:share:timeline", "menuItem:share:qq"] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
+        });
     });
 
     wx.error(function(res){
